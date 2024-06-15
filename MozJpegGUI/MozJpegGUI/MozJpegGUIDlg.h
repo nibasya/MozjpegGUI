7bf5fd1b5af6e5b336033c64219109525d8ce5db
donadona <15785731+nibasya@users.noreply.github.com>
2024-06-15 20:43:57 +0900

    grouped variables / functions in the header file.

--- MozJpegGUI/MozJpegGUI/MozJpegGUIDlg.h
+++ MozJpegGUI/MozJpegGUI/MozJpegGUIDlg.h
@@ -34,10 +34,18 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
+	// File controls
 	CListBox m_CtrlListFileList;
 	CButton m_CtrlButtonAddFolder;
 	CButton m_CtrlButtonConvert;
 	CButton m_CtrlButtonClear;
+	CButton m_CtrlCheckOverwrite;
+	CButton m_CtrlCheckCopyIfSmaller;
+	CButton m_CtrlCheckSaveToOriginalFolder;
+	CButton m_CtrlButtonSaveTo;
+	CEdit m_CtrlEditSaveTo;
+
+	// Convert options
 	CComboBox m_CtrlComboSetting;
 	CEdit m_CtrlEditQuality;
 	CComboBox m_CtrlComboColor;
@@ -48,21 +56,23 @@ public:
 	CButton m_CtrlCheckTrellisOptimization;
 	CButton m_CtrlCheckTrellisOptimizationOfDCCoefficients;
 	CComboBox m_CtrlComboTuneTrellisOptimization;
-	// Initialize convertion setting
-	CButton m_CtrlButtonInitialize;
+
+	// misc. controls
+	CButton m_CtrlButtonInitialize;	// Initialize convertion setting
 	CToolTipCtrl m_CtrlToolTip;
+
+	// other variables
+	CProgressDlg* m_pProgressDlg;
+	int m_CurrentSetting;	// Current setting number used for mozJpegGUI setting
+	int m_SelectedSetting;	// Selected setting number in m_CtrlComboSetting
+
+	// MFC functions
 	virtual BOOL PreTranslateMessage(MSG* pMsg);
 	afx_msg void OnDropFiles(HDROP hDropInfo);
-	// Returns true if the file can be handled by this software. filename can include its path.
-	bool CheckFileExtern(CString filename);
+	bool CheckFileExtern(CString filename);	// Returns true if the file can be handled by this software. The filename can include its path.
 	afx_msg void OnDestroy();
-	afx_msg void OnBnClickedButtonClear();
-	afx_msg void OnBnClickedButtonAddfolder();
-	CEdit m_CtrlEditSaveTo;
-	afx_msg void OnBnClickedButtonSaveto();
-	afx_msg void OnBnClickedButtonConvert();
-	CProgressDlg* m_pProgressDlg;
-	CButton m_CtrlCheckOverwrite;
+
+	// support functions
 	void ReadSetting();
 	void SaveSetting();
 	void ReadConvertSetting(int num);
@@ -70,22 +80,21 @@ public:
 	int InitSettingCombo();
 	void InitGUI();
 	void CreateInitialSetting();
+	void CreateInitialSettingSub(int num);
+	CString CreateOptions();
+	static UINT WaitForAbort(LPVOID pData);	// Wait for prgress control to complete abort
+
+
+	// GUI functions
+	afx_msg void OnBnClickedButtonClear();
+	afx_msg void OnBnClickedButtonAddfolder();
+	afx_msg void OnBnClickedButtonSaveto();
+	afx_msg void OnBnClickedButtonConvert();
 	afx_msg void OnBnClickedButtonSettingLoad();
 	afx_msg void OnBnClickedButtonSettingSave();
 	afx_msg void OnBnClickedButtonInitialize();
-	void CreateInitialSettingSub(int num);
-	CString CreateOptions();
 	afx_msg void OnClose();
-	// Wait for prgress control to complete abort
-	static UINT WaitForAbort(LPVOID pData);
-	CButton m_CtrlCheckCopyIfSmaller;
 	afx_msg void OnCbnCloseupComboSetting();
-	// Current setting number used for mozJpegGUI setting
-	int m_CurrentSetting;
-	// Selected setting number in m_CtrlComboSetting
-	int m_SelectedSetting;
 	afx_msg void OnCbnDropdownComboSetting();
-	CButton m_CtrlCheckSaveToOriginalFolder;
 	afx_msg void OnBnClickedCheckSaveToOriginalFolder();
-	CButton m_CtrlButtonSaveTo;
 };
